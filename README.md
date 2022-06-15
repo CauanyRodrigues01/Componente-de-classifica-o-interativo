abfea73968a409ade5c510401b478deb5901ebaa
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2022-06-14 22:58:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Componentes-de-classificao-interativo
+ 
---
+++ public/js/script.js
@@ -0,0 +1,3 @@
+const rating = document.querySelector("#rating");
+const thank = document.querySelector("#thank");
+const btn_submit = document.querySelector(".btn_submit")
