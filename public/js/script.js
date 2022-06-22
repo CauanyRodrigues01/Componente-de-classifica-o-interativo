b73b6da59dab956bf3f39789a9800f79e48b88f0
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2022-06-22 20:32:25 -0300

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
 const rating = document.querySelector("#rating");
 const thank = document.querySelector("#thank");
 const btn_submit = document.querySelector(".btn-submit")
+
+btn_submit.addEventListener('click', função);
